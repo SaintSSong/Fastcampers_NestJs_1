73d7e1cff82c78e9719b35606cb76a72fc874fe9
songsamuel <songsme2886@gmail.com>
2026-01-19 00:18:50 +0900

    기초 끝

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
@@ -6,6 +7,7 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 
@@ -27,12 +29,18 @@ export class AppController {
     },
   ];
 
+  private idCounter = 3;
+
   constructor(private readonly appService: AppService) {}
 
   // @Get('movie') // <- 같은 get이여도 데코레이터(@)에 엔드포인트를 집어 넣으면 해당 엔드포인트로 집결된다.
   @Get() // <- 이게 왜 되냐? 4번 행에서 컨트롤러 속에 "movie"를 넣었기 때문에 기본적으로 엔드포인트가 "movie"가 된다.
-  getMovies() {
-    return this.movies;
+  getMovies(@Query('title') title?: string) {
+    if (!title) {
+      return this.movies;
+    }
+
+    return this.movies.filter((m) => m.title.startsWith(title)); // startsWith은 ~~로 시작한다.
   }
 
   @Get(':id')
@@ -47,25 +55,47 @@ export class AppController {
   }
 
   @Post()
-  postMovie() {
-    return {
-      id: 3,
-      name: '어벤져스',
-      character: ['아이언맨, 캡틴 아메리카'],
+  postMovie(
+    @Body('title') // <- body값으로 받을 key를 () 속에 넣는 것이다.
+    title: string, // <- body값의 value의 속성을 의미한다.
+  ) {
+    const movie: Movie = {
+      id: this.idCounter++,
+      title: title,
     };
+
+    this.movies.push(movie);
+
+    return movie;
   }
 
   @Patch(':id')
-  patchMovie() {
-    return {
-      id: 3,
-      name: '어벤져스',
-      character: ['아이언맨, 블랙 위도우'],
-    };
+  patchMovie(@Param('id') id: string, @Body('title') title: string) {
+    const movie = this.movies.find((m) => m.id === +id);
+
+    if (!movie) {
+      throw new NotFoundException('존재하지 않는 영화입니다.'); // NotFoundException는 Nest에서 제공하는 메서드다.
+    }
+
+    // 찾은 영화에 타이틀을 덮어씌우는 코드
+    // Object.assign은 Js 문법이다.
+    Object.assign(movie, { title });
+
+    return movie;
   }
 
   @Delete(':id')
-  deleteMovie() {
-    return 3;
+  deleteMovie(@Param('id') id: string) {
+    const movieIndex = this.movies.findIndex((m) => m.id === +id);
+
+    if (movieIndex === -1) {
+      // <- !movieIndex는 id가 0이 나올 수 있으니까 안된다. findIndex는 positive만 나오기 때문에 -1로 넣어야한다.
+      throw new NotFoundException('존재하지 않는 영화입니다.'); // NotFoundException는 Nest에서 제공하는 메서드다.
+    }
+
+    // 배열에서 빼는 함수 사용해야한다.
+    this.movies.splice(movieIndex, 1);
+
+    return id;
   }
 }
