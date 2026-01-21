bbaac04e39130f9c01e7561e1c67962841c97c77
songsamuel <samuelsong@digiforet.com>
2026-01-21 18:53:43 +0900

    회사에서 하던 것

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,47 +11,20 @@ import {
 } from '@nestjs/common';
 import { AppService } from './app.service';
 
-interface Movie {
-  id: number;
-  title: string;
-}
-
 @Controller('movie') // <- 공통적으로 사용되는 엔드포인트는 여기다가 집어 넣는다.
 export class AppController {
-  private movies: Movie[] = [
-    {
-      id: 1,
-      title: '해리포터',
-    },
-    {
-      id: 2,
-      title: '반지의 제왕',
-    },
-  ];
-
-  private idCounter = 3;
-
   constructor(private readonly appService: AppService) {}
 
   // @Get('movie') // <- 같은 get이여도 데코레이터(@)에 엔드포인트를 집어 넣으면 해당 엔드포인트로 집결된다.
   @Get() // <- 이게 왜 되냐? 4번 행에서 컨트롤러 속에 "movie"를 넣었기 때문에 기본적으로 엔드포인트가 "movie"가 된다.
   getMovies(@Query('title') title?: string) {
-    if (!title) {
-      return this.movies;
-    }
-
-    return this.movies.filter((m) => m.title.startsWith(title)); // startsWith은 ~~로 시작한다.
+    console.log('여기');
+    return this.appService.getManyMovies(title);
   }
 
   @Get(':id')
   getMovie(@Param('id') id: string) {
-    const movie = this.movies.find((m) => m.id === +id);
-
-    if (!movie) {
-      throw new NotFoundException('존재하지 않는 영화입니다.'); // NotFoundException는 Nest에서 제공하는 메서드다.
-    }
-
-    return movie;
+    return this.appService.getMovieById(+id);
   }
 
   @Post()
@@ -59,43 +32,16 @@ export class AppController {
     @Body('title') // <- body값으로 받을 key를 () 속에 넣는 것이다.
     title: string, // <- body값의 value의 속성을 의미한다.
   ) {
-    const movie: Movie = {
-      id: this.idCounter++,
-      title: title,
-    };
-
-    this.movies.push(movie);
-
-    return movie;
+    return this.appService.createMovie(title);
   }
 
   @Patch(':id')
   patchMovie(@Param('id') id: string, @Body('title') title: string) {
-    const movie = this.movies.find((m) => m.id === +id);
-
-    if (!movie) {
-      throw new NotFoundException('존재하지 않는 영화입니다.'); // NotFoundException는 Nest에서 제공하는 메서드다.
-    }
-
-    // 찾은 영화에 타이틀을 덮어씌우는 코드
-    // Object.assign은 Js 문법이다.
-    Object.assign(movie, { title });
-
-    return movie;
+    return this.appService.updateMovie(+id, title);
   }
 
   @Delete(':id')
   deleteMovie(@Param('id') id: string) {
-    const movieIndex = this.movies.findIndex((m) => m.id === +id);
-
-    if (movieIndex === -1) {
-      // <- !movieIndex는 id가 0이 나올 수 있으니까 안된다. findIndex는 positive만 나오기 때문에 -1로 넣어야한다.
-      throw new NotFoundException('존재하지 않는 영화입니다.'); // NotFoundException는 Nest에서 제공하는 메서드다.
-    }
-
-    // 배열에서 빼는 함수 사용해야한다.
-    this.movies.splice(movieIndex, 1);
-
-    return id;
+    return this.appService.deleteMovie(+id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,81 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+export interface Movie {
+  id: number;
+  title: string;
+}
 
 @Injectable()
+// @Injectable() <- 이렇게 놓으면 IoC 컨테이너한테 너가 이 class를 IoC 컨테이너에서 알아서 관리해줘 라고 명령하는 거랑 똑같다.
+// 그러면 IoC 컨테이너는 AppService를 인스턴스화 해서 내가 다른 곳에서 AppService를 필요로 하는 곳에서 인스턴스화 했으니까 자동으로 넣어줄게! 이러게 된다.
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private movies: Movie[] = [
+    {
+      id: 1,
+      title: '해리포터',
+    },
+    {
+      id: 2,
+      title: '반지의 제왕',
+    },
+  ];
+
+  private idCounter = 3;
+
+  getManyMovies(title?: string) {
+    if (!title) {
+      return this.movies;
+    }
+
+    return this.movies.filter((m) => m.title.startsWith(title)); // startsWith은 ~~로 시작한다.
+  }
+
+  getMovieById(id: number) {
+    const movie = this.movies.find((m) => m.id === +id);
+
+    if (!movie) {
+      throw new NotFoundException('존재하지 않는 영화입니다.'); // NotFoundException는 Nest에서 제공하는 메서드다.
+    }
+
+    return movie;
+  }
+
+  createMovie(title: string) {
+    const movie: Movie = {
+      id: this.idCounter++,
+      title: title,
+    };
+
+    this.movies.push(movie);
+
+    return movie;
+  }
+
+  updateMovie(id: number, title: string) {
+    const movie = this.movies.find((m) => m.id === +id);
+
+    if (!movie) {
+      throw new NotFoundException('존재하지 않는 영화입니다.'); // NotFoundException는 Nest에서 제공하는 메서드다.
+    }
+
+    // 찾은 영화에 타이틀을 덮어씌우는 코드
+    // Object.assign은 Js 문법이다.
+    Object.assign(movie, { title });
+
+    return movie;
+  }
+
+  deleteMovie(id: number) {
+    const movieIndex = this.movies.findIndex((m) => m.id === +id);
+
+    if (movieIndex === -1) {
+      // <- !movieIndex는 id가 0이 나올 수 있으니까 안된다. findIndex는 positive만 나오기 때문에 -1로 넣어야한다.
+      throw new NotFoundException('존재하지 않는 영화입니다.'); // NotFoundException는 Nest에서 제공하는 메서드다.
+    }
+
+    // 배열에서 빼는 함수 사용해야한다.
+    this.movies.splice(movieIndex, 1);
+
+    return id;
   }
 }
